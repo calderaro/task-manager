420a4b8583ff2f5813d49bbdcd05cdcae602cac7
Angel calderaro <calderaro94@gmail.com>
2018-01-19 00:37:25 -0600

    clean routes

--- src/containers/Root/routes.js
+++ src/containers/Root/routes.js
@@ -3,12 +3,7 @@ import * as Routes from './SyncRoutes'
 const routes = [
   {path: '/', exact: true, component: Routes.Home},
   {path: '/registro', exact: true, component: Routes.Register},
-  {path: '/login', exact: true, component: Routes.Login},
-  {path: '/conocenos', exact: true, component: Routes.About},
-  {path: '/audios', exact: true, component: Routes.Audios},
-  {path: '/videos', exact: true, component: Routes.Videos},
-  {path: '/obras', exact: true, component: Routes.Works},
-  {path: '/exposiciones', exact: true, component: Routes.Exhibitions}
+  {path: '/login', exact: true, component: Routes.Login}
 ]
 
 export default routes
