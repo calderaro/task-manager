c39839739db762c4fc6641ac066281a09e971019
Angel calderaro <calderaro94@gmail.com>
2018-01-21 03:26:55 -0600

    update header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,17 +8,9 @@ const Navbar = (props) => {
     <header className={navOut ? style.header + ' ' + style.headerOut : style.header}>
       <nav className={style.nav}>
         <div>
-          <Link to='/'>
-            <img src='/static/img/inbateca.svg' />
-          </Link>
-          <Link to='/conocenos'>CONÓCENOS</Link>
-          <Link to='/exposiciones'>EXPOSICIONES</Link>
-          <Link to='/obras'>OBRAS</Link>
-          <Link to='/audios'>AUDIOS</Link>
-          <Link to='/videos'>VIDEOS</Link>
+          <a>TASK MANAGER</a>
         </div>
         <div>
-          <button><i className='fa fa-search' /></button>
           <Link to='/login'>Registro / Login</Link>
         </div>
       </nav>
